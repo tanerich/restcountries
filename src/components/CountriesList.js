82ed7f9d5543a86193e4eb3ae66f394211084554
IsmailOu-sekou <ismailousekou@gmail.com>
2024-07-09 16:03:52 +0200

    added a select region/reset button and a state segment

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 
 const CountriesList = () => {
 
 const[countries, setCountries] = useState([]);
+const [segment, setSegment] = useState("all")
 const searchInput = useRef("");
 
 const getCountries = async() => {
 
     
     try {
-      const result = await axios.get("https://restcountries.com/v3.1/all");
+      const result = await axios.get(`https://restcountries.com/v3.1/${segment}`);
 
       if(result.status === 200) {
 
@@ -26,49 +27,56 @@ const getCountries = async() => {
 }
 
 const searchCountries = async(e) => {
-
-e.preventDefault();
-
-   let search = searchInput.current.value;
-
-   if(search === "") {
-
-    alert ("Please write something!!")
-    return;
-   }
+  
+  e.preventDefault();
+  
+  let search = searchInput.current.value;
+  
+  if(search === "") {
     
-  try {
-    const result = await axios.get(`https://restcountries.com/v3.1/name/${search}`);
-
-    if(result.status === 200) {
-
-      setCountries(result.data)
-    };
-
-
-  } catch (error) {
-      console.error(error);
+    alert("Please write something!!")
+    return;
   }
+  
+ setSegment(`name/${search}`)
+  
+}
+
+const reset = () => {
+
+  searchInput.current.value= ""
+  setSegment("all")
 
 }
 
 useEffect(() => {
 
   getCountries();
-}, [])
+}, [segment]);
 
 
 
   return (
     <>
-    <div className="row my-5">
+    <div className="row my-5 ">
       <div className="col-md-8">
           <h1>List of Countries</h1>
       </div>
-        <div className="col-md-4">
+        <div className="col-md-4 ">
+
         <form onSubmit={searchCountries}   className="d-flex">
+        <select>
+          <option value="">Select a country</option>
+          <option value="americas">Americas</option>
+          <option value="africa">Africa</option>
+          <option value="asia">Asia</option>
+          <option value="oceania">Oceania</option>
+        </select>
         <input ref={searchInput} className="form-control me-sm-2" type="search" placeholder="Search"/>
         <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        {segment !== "all" ?  (
+       <button onClick={reset} className="btn btn-secondary my-2 my-sm-0" type="delete">Reset</button>
+        ): ""}
       </form>
         </div>
     </div>
